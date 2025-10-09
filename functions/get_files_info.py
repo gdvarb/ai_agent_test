b63efba9c0acc1e76a2d870b64dfb024950dfacd
Georgi Varbanov <gvarbanov@usf.edu>
2025-10-09 14:08:07 -0400

    add tests for get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,33 @@
 import os
 def get_files_info(working_directory, directory="."):
     working_absolute_path = os.path.abspath(working_directory)
+    #print("debug working_directory:", working_absolute_path)
+    
 
-    directory_absolute_path = os.path.abspath(directory)
-    if not os.path.isdir(directory_absolute_path):
+    full_path = os.path.abspath(os.path.join(working_absolute_path, directory))
+
+    #print("abs_path_dir:",full_path)
+    
+    #print("isdir full_path:", os.path.isdir(full_path))
+    #print("abs_path_dir:",os.path.abspath(full_path))
+
+    #if directory == "." or directory in os.listdir(working_absolute_path):
+    #    directory_absolute_path = os.path.join(working_absolute_path, directory)
+
+
+    if not os.path.isdir(full_path):
         print(f'Error: "{directory}" is not a directory')
         return f'Error: "{directory}" is not a directory'
-        
-
-    full_path = os.path.join(working_directory, directory_absolute_path)
+            
+    
     
     if not full_path.startswith(working_absolute_path):
         print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     else:
-        print(f'"{directory}" is a valid working directory')
+        #print(f'"{directory}" is a valid working directory')
         files =os.listdir(full_path)
-        print(files)
+        #print(files)
         file_string = ""
         for file in files:
             if file.startswith("."):
