37884741c8ed46193997ffb73364e0b233b226ef
Georgi Varbanov <gvarbanov@usf.edu>
2025-10-10 15:29:45 -0400

    add function to run python files and slight reformatting

--- functions/tools.py
+++ functions/tools.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import subprocess
 
 def get_file_content(working_directory, file_path):
     MAX_CHARS = 10000
@@ -58,16 +60,63 @@ def write_file(working_directory, file_path, content):
     if not full_path.startswith(working_absolute_path):
         print(f'Error: Cannot write to "{file_path}" as it is outside of the permitted working directory')
         return f'Error: Cannot write to "{file_path}" as it is outside of the permitted working directory'
-    if not os.path.exists(full_path):
-        os.makedirs(full_path)
-        print(f'Made directory: {full_path}')
+
+    dirname = os.path.dirname(full_path)
+    #print(f'Directory name is: {dirname}')
+
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+        #print(f'Made directory: {dirname}')
 
     try:
         with open(full_path, 'w') as f:
             f.write(content + '\n')
-            print(f'Successfully wrote to "{file_path}" ({len(content)} characters writen)')
+            #print(f'Successfully wrote to "{file_path}" ({len(content)} characters writen)')
+            print(f"'{len(content)} characters written'")
+            return f"'{len(content)} characters written'"
     except Exception as e:
-        print(f'An error occured: {e}')
-        return f'An error occured: {e}'
+        print(f"'Error:'")
+        return f"'An error occured: {e}'"
 
     
+def run_python_file(working_directory, file_path, args=[]):
+    working_absolute_path = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not full_path.startswith(working_absolute_path):
+        print(f'Error: Cannot execute "{file_path}" as it is outside of the permitted working directory')
+        return f'Error: Cannot execute "{file_path}" as it is outside of the permitted working directory'
+
+    if not os.path.isfile(full_path):
+        print(f'Error: File "{file_path}" not found')
+        return f'Error: File "{file_path}" not found'
+
+    
+    dirname = os.path.dirname(full_path)
+    if not os.path.exists(dirname):
+        print(f'Error: "{file_path} not found."') 
+        return f'Error: "{file_path} not found."'
+
+    #print("Full path:",full_path)
+    if not full_path.endswith(".py"):
+        print(f'Error: "{file_path}" is not a Python file.')
+        return f'Error: "{file_path}" is not a Python file.'
+
+    function_args = [sys.executable]
+    function_args.extend([full_path])
+    function_args.extend(args)
+    #print('Function args:',function_args)
+    try:
+        function_output = subprocess.run(function_args, capture_output=True, timeout=30, text=True)
+        if not function_output:
+            print("No output produced.")
+
+        if function_output.stdout:
+            print(f'STDOUT: {function_output.stdout}')
+        elif function_output.stderr:
+            print(f'STDERR: {function_output.stderr}')
+
+    except Exception as e:
+        print(f'Error: executing Python file: {e}')
+
+
--- tests.py
+++ tests.py
@@ -22,12 +22,17 @@ from functions.tools import *
 #get_file_content("calculator", 'pkg/does_not_exist.py')
 
 
-print("Results for write calculator, lorem.txt")
-write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-print("Results for write calculator/pkg/morelorem.txt")
-write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-print("Results for write calculator/tmp/temp.txt")
-write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+#print("Results for write calculator, lorem.txt")
+#write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+#print("Results for write calculator/pkg/morelorem.txt")
+#write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+#print("Results for write calculator/tmp/temp.txt")
+#write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
 
 
+run_python_file("calculator", "main.py")
+run_python_file("calculator", "main.py", ["3 + 5"])
+run_python_file("calculator", "tests.py")
+run_python_file("calculator", "../main.py")
+run_python_file("calculator", "nonexistent.py")
 
