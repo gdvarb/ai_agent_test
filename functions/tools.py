3d115f0873e00d718056bf3bb7b6c3dad0f79223
Georgi Varbanov <gvarbanov@usf.edu>
2025-10-15 15:37:45 -0400

    Giving agent access to tools. Not fully working

--- functions/tools.py
+++ functions/tools.py
@@ -1,6 +1,22 @@
 import os
 import sys
 import subprocess
+from google import genai
+from google.genai import types
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself."
+            ),
+        },
+    ),
+)
 
 def get_file_content(working_directory, file_path):
     MAX_CHARS = 10000
--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import sys
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from system_prompt import system_prompt
+from functions.tools import schema_get_files_info
+
+
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+model = "gemini-2.0-flash-001"
 
 
 def main():
@@ -15,8 +28,22 @@ def main():
         messages = [
             types.Content(role="user", parts=[types.Part(text=user_prompt)]),
         ]
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+        response = client.models.generate_content(
+            model=model,
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt),
+        )
         try:
+            print(response.function_calls)
+            if response.function_calls: 
+                function_call_part = response.function_calls[0]
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+                function_res = function_call_part.name
+                print(response.text)
+
+                return f"Calling function: {function_call_part.name}({function_call_part.args})"
             if sys.argv[2] and sys.argv[2] == "--verbose":
                 print(f"User prompt: {user_prompt} \n Response: {response.text} \n Prompt tokens: {response.usage_metadata.prompt_token_count} \n Response tokens: {response.usage_metadata.candidates_token_count}") 
         except:
